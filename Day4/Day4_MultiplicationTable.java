e2e197ca012a9b2e498229111d24547db01abc8a
samiyark01 <samiyarkhan12@gmail.com>
2026-04-12 23:05:31 +0530

    Create Day4_MultiplicationTable.java

---
+++ Day4/Day4_MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Solution
+{
+    
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int num= sc.nextInt();
+        for(int i =1; i<=10; i++)
+        {
+            System.out.println(num  + " x " +   i +   " = " +   ( num*i));
+        }
+    }
+}
